6b2636aaaa0d0370c8ae3412492fb484fffebc6c
daniel1408 <daniel-costa@ufu.br>
2017-10-24 18:29:46 -0200

    Atualização da Listagem

---
+++ netcore-movie/Controllers/CastController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using texas_movie.Models;
+
+namespace texas_movie.Controllers
+{
+    public class CastController : Controller
+    {
+        public IActionResult Cast()
+        {
+            return View();
+        }
+    }
+}
