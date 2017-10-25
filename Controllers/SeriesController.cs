5aa8aea186f519d11dc0c80cb0df49bd5c67f791
daniel1408 <daniel-costa@ufu.br>
2017-10-24 23:50:57 -0200

    Update

--- Controllers/SeriesController.cs
+++ Controllers/SeriesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -12,7 +12,54 @@ namespace texas_movie.Controllers
     {
         public IActionResult Listar()
         {
-            return View();
+            Series[] Series = new Series[] 
+            {
+                new Series
+                    {
+                        Id = 1,
+                        Nome = "The Americans",
+                        Nota = 8
+                    },
+                new Series
+                    {
+                        Id = 2,
+                        Nome = "The Big Bang Theory",
+                        Nota = 9
+                    },
+                new Series
+                    {
+                        Id = 3,
+                        Nome = "The Black List",
+                        Nota = 8
+                    },
+                new Series
+                    {
+                        Id = 4,
+                        Nome = "Chuck",
+                        Nota = 7
+                    },
+                new Series
+                    {
+                        Id = 5,
+                        Nome = "Lethal Weapon",
+                        Nota = 9
+                    },
+                new Series
+                    {
+                        Id = 6,
+                        Nome = "How i Meet Your Mother",
+                        Nota = 8
+                    },
+                new Series
+                    {
+                        Id = 7,
+                        Nome = "Quantic",
+                        Nota = 7
+                    },
+
+            };
+
+            return View(Series);
         }
 
         public IActionResult Criar()
